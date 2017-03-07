50d97f6b4126f7b6a94ddfbb8e62ded27285304b
Caio Begotti <caio1982@gmail.com>
2017-03-07 17:13:30 -0300

    get the registry running after deployed, on xenial

--- docker-registry/README.markdown
+++ docker-registry/README.markdown
@@ -1,9 +1,29 @@
 # docker-registry-charm
 Docker registry charm for Juju, to be used with the Canonical Distribution of Kubernetes (CDK):
 
+Build the charm (until it is published to the charm store) with:
+
 ```
-charm build --series trusty
+charm build --series xenial
+```
+
+Deploy it locally with:
+
+```
+juju deploy ./xenial/docker-registry --series xenial
+```
+
+If you cannot pull upstream images to install the registry, you can use a resource:
+
+```
 docker pull registry:latest
 docker save -o /tmp/registry.tar registry:latest
-juju deploy ./trusty/docker-registry --series trusty --resource registry=/tmp/registry.tar
+juju deploy ./xenial/docker-registry --series xenial --resource registry=/tmp/registry.tar
+```
+
+Verify the Docker registry responds after deploying it:
+
+```
+juju expose docker-registry
+curl -X GET http://<docker_registry_ip_address>:5000/v2/_catalog
 ```
--- docker-registry/reactive/docker_registry.py
+++ docker-registry/reactive/docker_registry.py
@@ -3,11 +3,15 @@ from charmhelpers.core.hookenv import open_port
 from charmhelpers.core.hookenv import close_port
 from charmhelpers.core.hookenv import status_set
 from charmhelpers.core.hookenv import resource_get
+
+from charms.docker import Compose
+
 from charms.reactive import when, when_not, set_state
 from charms.reactive import when_any
 from charms.reactive import remove_state
 
-from subprocess import check_output, check_call
+from charms.templating.jinja2 import render
+from subprocess import check_call
 
 
 @when('docker.available')
@@ -16,27 +20,38 @@ from subprocess import check_output, check_call
 def launch_standalone_formation():
     path = resource_get('registry')
     if path:
-        if not check_call(['docker', 'load', '-i', path]):
-            status_set('maintenance', 'Cannot import registry resource.')
-            return
+        check_call(['docker', 'load', '-i', path])
+
+    render('docker-compose.yml',
+           'files/docker-registry/docker-compose.yml',
+           config())
 
     start_application()
     set_state('docker-registry.standalone.running')
     status_set('active', 'Docker registry ready.')
 
 
-@when_any('config.changed.registry_port')
+@when_any('config.changed.registry_port', 'config.changed.registry_tag')
 def recycle_networking_and_app():
     cfg = config()
     # guard on first run, no previous values, so do nothing
-    if not cfg.previous('registry_port'):
+    if not cfg.previous('registry_port') or not cfg.previous('registry_tag'):
         return
 
     status_set('maintenance', 'Re-configuring port bindings.')
     close_port(cfg.previous('registry_port'))
+
+    status_set('maintenance', 'Re-generating Docker compose YAML.')
+    render('docker-compose.yml',
+           'files/docker-registry/docker-compose.yml',
+           config())
+
     remove_state('docker-registry.standalone.running')
     remove_state('docker-registry.running')
 
 
 def start_application():
+    compose = Compose('files/docker-registry')
+    compose.up()
+
     open_port(config('registry_port'))
