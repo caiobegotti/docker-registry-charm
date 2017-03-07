d5bebe6fe53b8c10fa82b46480437df9e0e0e15e
Caio Begotti <caio1982@gmail.com>
2017-03-07 15:20:32 -0300

    fixes and cleanups

--- docker-registry/reactive/docker_registry.py
+++ docker-registry/reactive/docker_registry.py
@@ -1,43 +1,45 @@
+from charmhelpers.core.hookenv import log
 from charmhelpers.core.hookenv import config
 from charmhelpers.core.hookenv import open_port
 from charmhelpers.core.hookenv import close_port
 from charmhelpers.core.hookenv import status_set
+from charmhelpers.core.hookenv import resource_get
 from charms.reactive import when, when_not, set_state
 from charms.reactive import when_any
 from charms.reactive import remove_state
+
+from subprocess import check_output, check_call
 
 
 @when('docker.available')
 @when_not('docker-registry.standalone.running')
 @when_not('block_standalone')
 def launch_standalone_formation():
-    """ By default we want to execute the stand-alone formation """
+    path = resource_get('registry')
+    if path:
+        if not check_call(['docker', 'load', '-i', path]):
+            return
+        images = check_output(['docker', 'images'])
+        log(images, 'INFO')
 
-    # Start our application, and open the ports
     start_application()
-    # Set our idempotency state
     set_state('docker-registry.standalone.running')
-    status_set('active', 'Docker registry ready')
+    status_msg = 'Docker registry ready'
+    status_set('active', status_msg)
 
 
 @when_any('config.changed.registry_port')
 def recycle_networking_and_app():
     cfg = config()
-    # guard on first run, no previous values, so do nothing.
+    # guard on first run, no previous values, so do nothing
     if not cfg.previous('registry_port'):
         return
 
-    # Close previously configured ports
     status_set('maintenance', 'Re-configuring port bindings.')
     close_port(cfg.previous('registry_port'))
-    # as the open port and app spinup are in another method, consume
-    # that and tell juju to re-execute that method body by removing
-    # the idempotency state
     remove_state('docker-registry.standalone.running')
     remove_state('docker-registry.running')
 
 
 def start_application():
-    # Declare ports to Juju - this requires a manual step of juju expose
-    # otherwise the public facing ports never actually get exposed for traffic.
     open_port(config('registry_port'))
